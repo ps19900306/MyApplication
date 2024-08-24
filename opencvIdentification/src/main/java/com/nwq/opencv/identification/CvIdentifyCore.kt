80a7b55946cf65f709279fd59c3b1e9511b5ad21
gongduoxiang <gongduoxiang@163.com>
2024-08-24 14:58:18 +0800

    新建了一些模块

--- opencvIdentification/src/main/java/com/nwq/opencv/identification/CvIdentifyCore.kt
+++ opencvIdentification/src/main/java/com/nwq/opencv/identification/CvIdentifyCore.kt
@@ -1,28 +1,28 @@
 package com.nwq.opencv.identification
 
+import org.opencv.core.Core
+import org.opencv.core.Mat
+
 class CvIdentifyCore {
 
-//    // 存储最近几张图像用于比较
-//    private val recentImages = mutableListOf<Mat>()
-//
-//    private fun isSignificantChange(currentMat: Mat): Boolean {
-//        // 限制最近图像的数量
-//        if (recentImages.size > 3) {
-//            recentImages.removeAt(0)
-//        }
-//
-//        // 如果没有足够的图像进行比较，则认为变化显著
-//        if (recentImages.size < 3) return true
-//
-//        // 计算与前三张图像的MSE
-//        var totalMse = 0.0
-//        for (recentMat in recentImages) {
-//            totalMse += calculateMse(recentMat, currentMat)
-//        }
-//        val avgMse = totalMse / recentImages.size
-//
-//        // 设置阈值
-//        val threshold = 100.0
-//        return avgMse > threshold
-//    }
+    // 存储最近几张图像用于比较
+    private val recentImages = mutableListOf<Mat>()
+    private val diffThreshold = 1000.0 // 根据实际需求调整阈值
+
+
+    private fun shouldDetect(currentFrame: Mat): Boolean {
+        if (recentImages.size < 3) return true
+
+        var diffSum = 0.0
+        for (prev in recentImages) {
+            val diff = Mat()
+            Core.absdiff(currentFrame, prev, diff)
+            diffSum += Core.sumElems(diff).`val`[0]
+        }
+
+        return diffSum / recentImages.size > diffThreshold
+    }
+
+
+
 }
