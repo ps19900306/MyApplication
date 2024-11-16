1dbc57214bf5ba5d050d2e3c0d5ea09cc8fb1687
gongduoxiang <gongduoxiang@163.com>
2024-11-16 15:20:29 +0800

    验证规则进行修改

--- opencvIdentification/src/main/java/com/nwq/opencv/rgb/DifferencePointRule.kt
+++ opencvIdentification/src/main/java/com/nwq/opencv/rgb/DifferencePointRule.kt
@@ -11,74 +11,9 @@ class DifferencePointRule(
     val blueDifference: Int,
 ) {
 
-    fun verificationRule(
-        red1: Int, green1: Int, blue1: Int, red2: Int, green2: Int, blue2: Int
-    ): Boolean {
-        val flag1 = when {
-            redDifference > 0 -> {
-                if (red1 > redDifference) {
-                    red1 - red2 >= redDifference
-                } else {
-                    red1 - red2 >= red1 * 0.2
-                }
-            }
-
-            redDifference == 0 -> true
-            else -> { // redDifference < 0
-                if (red2 > -redDifference) {
-                    red1 - red2 <= redDifference
-                } else {
-                    red1 - red2 <= red2 * 0.2
-                }
-            }
-        }
-
-        if (!flag1) return false
-
-        val flag2 = when {
-            greenDifference > 0 -> {
-                if (green1 > greenDifference) {
-                    green1 - green2 >= greenDifference
-                } else {
-                    green1 - green2 >= green1 * 0.2
-                }
-            }
-
-            greenDifference == 0 -> true
-            else -> { // greenDifference < 0
-                if (green2 > -greenDifference) {
-                    green1 - green2 <= greenDifference
-                } else {
-                    green1 - green2 <= green2 * 0.2
-                }
-            }
-        }
-
-        if (!flag2) return false
-        val flag3 = when {
-            blueDifference > 0 -> {
-                if (blue1 > blueDifference) {
-                    blue1 - blue2 >= blueDifference
-                } else {
-                    blue1 - blue2 >= blue1 * 0.2
-                }
-            }
-
-            blueDifference == 0 -> true
-            else -> { // blueDifference < 0
-                if (blue2 > -blueDifference) {
-                    blue1 - blue2 <= blueDifference
-                } else {
-                    blue1 - blue2 <= blue2 * 0.2
-                }
-            }
-        }
-        return flag1 && flag2 && flag3
-    }
-
     companion object {
         private val list = mutableListOf<DifferencePointRule>()
-
+        private const val dFloat =0.4
         fun getSimple(
             red: Int,
             green: Int,
@@ -97,3 +32,70 @@ class DifferencePointRule(
             }
         }
     }
+
+
+    fun verificationRule(
+        red1: Int, green1: Int, blue1: Int, red2: Int, green2: Int, blue2: Int
+    ): Boolean {
+        val flag1 = when {
+            redDifference > 0 -> {
+                if (red1 > redDifference) {
+                    red1 - red2 >= redDifference
+                } else {
+                    red1 - red2 >= red1 * dFloat
+                }
+            }
+
+            redDifference == 0 -> true
+            else -> { // redDifference < 0
+                if (red2 > -redDifference) {
+                    red1 - red2 <= redDifference
+                } else {
+                    red1 - red2 <= red2 * dFloat
+                }
+            }
+        }
+
+        if (!flag1) return false
+
+        val flag2 = when {
+            greenDifference > 0 -> {
+                if (green1 > greenDifference) {
+                    green1 - green2 >= greenDifference
+                } else {
+                    green1 - green2 >= green1 * dFloat
+                }
+            }
+
+            greenDifference == 0 -> true
+            else -> { // greenDifference < 0
+                if (green2 > -greenDifference) {
+                    green1 - green2 <= greenDifference
+                } else {
+                    green1 - green2 <= green2 * dFloat
+                }
+            }
+        }
+
+        if (!flag2) return false
+        val flag3 = when {
+            blueDifference > 0 -> {
+                if (blue1 > blueDifference) {
+                    blue1 - blue2 >= blueDifference
+                } else {
+                    blue1 - blue2 >= blue1 * dFloat
+                }
+            }
+
+            blueDifference == 0 -> true
+            else -> { // blueDifference < 0
+                if (blue2 > -blueDifference) {
+                    blue1 - blue2 <= blueDifference
+                } else {
+                    blue1 - blue2 <= blue2 * dFloat
+                }
+            }
+        }
+        return flag1 && flag2 && flag3
+    }
+}
