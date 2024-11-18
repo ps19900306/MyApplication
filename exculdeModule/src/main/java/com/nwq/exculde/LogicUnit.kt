1cf0bbabe83bb5af4a5debf27077ae81febc6d4c
gongduoxiang <gongduoxiang@163.com>
2024-11-18 18:28:16 +0800

    考虑怎么写逻辑部分

--- exculdeModule/src/main/java/com/nwq/exculde/LogicUnit.kt
+++ exculdeModule/src/main/java/com/nwq/exculde/LogicUnit.kt
@@ -1,7 +1,23 @@
 package com.nwq.exculde
 
-class LogicUnit() {
+import com.nwq.baseobj.CoordinateArea
+import com.nwq.opencv.contract.FindTarget
 
 
+//判断单元 一个执行动作的判断单元
+abstract class LogicUnit() {
 
+    abstract val TAG: String
+    abstract val findTargetList: List<FindTarget>
+
+
+    suspend fun jude(): CoordinateArea? {
+        findTargetList.forEach {
+            val coordinateArea = it.findTarget()
+            if (coordinateArea != null) {
+                return coordinateArea
+            }
+        }
+        return null
+    }
 }
