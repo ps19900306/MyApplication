6ceb56060f20b2d7af6229c44cfd0eec6e904515
gongduoxiang <gongduoxiang@163.com>
2024-11-20 17:20:35 +0800

    新写了一个逻辑结构

--- exculdeModule/src/main/java/com/nwq/exculde/LogicUnit.kt
+++ exculdeModule/src/main/java/com/nwq/exculde/LogicUnit.kt
@@ -12,11 +12,17 @@ abstract class LogicUnit() {
 
     abstract val TAG: String
     abstract val findTargetList: List<FindTarget>
-    private var lastCoordinateArea: CoordinateArea? = null
-    private var clickArea: ClickArea? = null
     abstract val errorCount: Int
 
+    private var lastCoordinateArea: CoordinateArea? = null
+    private var clickArea: ClickArea? = null
+    private var nextList: List<LogicUnit>? = null
+    private var judeTime = -1
+    private var isEnd = false
     suspend fun jude(): Boolean {
+        if (judeTime == 0) {
+            return false
+        }
         findTargetList.forEach {
             val coordinateArea = it.findTarget()
             if (coordinateArea != null) {
@@ -29,21 +35,35 @@ abstract class LogicUnit() {
 
 
     //当本次jude()返回为True 时，入本方法  count连续进入次数  Boolean是否进行错误上报
-    suspend fun onJude(nowLogicUnitList: List<LogicUnit>, count: Int): Boolean {
+    open suspend fun onJude(nowLogicUnitList: List<LogicUnit>, count: Int): Boolean {
         if (errorCount in 1..<count) {
             return true;
         }
         clickArea?.let {
-            ClickBuilderUtils.buildClick(lastCoordinateArea!!, clickArea!!,0)
+            if (count % 2 == 1)
+                ClickBuilderUtils.buildClick(lastCoordinateArea!!, clickArea!!, 0)
         }
         return false
     }
 
     //当上一张图jude()返回为True时本张图进进入的不是次方法
-    abstract suspend fun hasChanged(nowLogicUnitList: List<LogicUnit>)
+    open suspend fun hasChanged(nowLogicUnitList: MutableList<LogicUnit>) {
+        // 如果存在下一个逻辑单元列表，则将其全部添加到当前列表中
+        nextList?.let {
+            nowLogicUnitList.addAll(it)
+        }
+        // 如果判断次数大于0，则减少判断次数
+        if (judeTime > 0) {
+            judeTime--
+        }
+        // 如果判断次数为0，表明已达到设定的判断次数，从当前列表中移除自身
+        if (judeTime == 0) {
+            nowLogicUnitList.remove(this)
+        }
+    }
 
     open suspend fun isEnd(): Boolean {
-        return false
+        return isEnd
     }
 
 }
