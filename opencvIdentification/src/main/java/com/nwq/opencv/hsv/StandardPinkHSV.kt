807724bce827f3ab9eeb54392935f213f941e8f8
gongduoxiang <gongduoxiang@163.com>
2024-11-22 17:59:09 +0800

    增加几个基础颜色

---
+++ opencvIdentification/src/main/java/com/nwq/opencv/hsv/StandardPinkHSV.kt
@@ -0,0 +1,4 @@
+package com.nwq.opencv.hsv
+
+class StandardPinkHSV : HSVRule(156, 170, 43, 255, 46, 255){
+}
--- opencvIdentification/src/main/java/com/nwq/opencv/hsv/StandardRed2HSV.kt
+++ opencvIdentification/src/main/java/com/nwq/opencv/hsv/StandardRed2HSV.kt
@@ -1,4 +1,4 @@
 package com.nwq.opencv.hsv
 
-class StandardRed2HSV : HSVRule(156, 180, 43, 255, 46, 255){
+class StandardRed2HSV : HSVRule(170, 180, 43, 255, 46, 255){
 }
