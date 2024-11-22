6b1e238eed3d424351ad8defb230a13dd52e055f
gongduoxiang <gongduoxiang@163.com>
2024-11-22 18:09:15 +0800

    增加几个基础颜色

---
+++ opencvIdentification/src/main/java/com/nwq/opencv/hsv/StandardBlueHSV.kt
@@ -0,0 +1,4 @@
+package com.nwq.opencv.hsv
+
+class StandardBlueHSV : HSVRule(100, 124, 43, 255, 46, 255){
+}
--- opencvIdentification/src/main/java/com/nwq/opencv/hsv/StandardBuleHSV.kt
+++
@@ -1,4 +0,0 @@
-package com.nwq.opencv.hsv
-
-class StandardBuleHSV : HSVRule(100, 124, 43, 255, 46, 255){
-}
