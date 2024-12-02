3dc385d4feaa1374982acf0a784dab0133a8e4b7
gongduoxiang <gongduoxiang@163.com>
2024-12-02 17:51:45 +0800

    设置数据信息

--- app/src/main/java/com/example/myapplication/adapter/FindTargetListAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/FindTargetListAdapter.kt
@@ -2,10 +2,15 @@ package com.example.myapplication.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 
 import com.example.myapplication.databinding.ItemFindTargetBinding
+import com.nwq.opencv.db.entity.FindTargetHsvEntity
+import com.nwq.opencv.db.entity.FindTargetImgEntity
+import com.nwq.opencv.db.entity.FindTargetMatEntity
 import com.nwq.opencv.db.entity.FindTargetRecord
+import com.nwq.opencv.db.entity.FindTargetRgbEntity
 
 
 class FindTargetListAdapter(
@@ -21,7 +26,6 @@ class FindTargetListAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         return ViewHolder(
             ItemFindTargetBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -32,14 +36,48 @@ class FindTargetListAdapter(
 
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FindTargetListAdapter.ViewHolder, position: Int) {
         val item = values[position]
+        holder.bindData(item)
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: ItemFindTargetBinding) :
+    inner class ViewHolder(val binding: ItemFindTargetBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        init {
+
+        }
+
+        fun bindData(item: FindTargetRecord) {
+
+            binding.rgbBtn.visibility = View.GONE
+            binding.hsvBtn.visibility = View.GONE
+            binding.imgBtn.visibility = View.GONE
+            binding.matBtn.visibility = View.GONE
+
+            item.list.forEach {
+                when (it) {
+                    is FindTargetRgbEntity -> {
+                        binding.rgbBtn.visibility = View.VISIBLE
+                    }
+
+                    is FindTargetHsvEntity -> {
+                        binding.hsvBtn.visibility = View.VISIBLE
+                    }
+
+                    is FindTargetImgEntity -> {
+                        binding.imgBtn.visibility = View.VISIBLE
+                    }
+
+                    is FindTargetMatEntity -> {
+                        binding.matBtn.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+
 
 
     }
