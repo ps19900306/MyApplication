e5516458273896af45b8f6875c9dc93bba41b165
gongduoxiang <gongduoxiang@163.com>
2024-12-09 18:21:08 +0800

    保存一波数据

--- app/src/main/java/com/example/myapplication/opencv/SelectRegionFragment.kt
+++ app/src/main/java/com/example/myapplication/opencv/SelectRegionFragment.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.opencv
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentSelectRegionBinding
 import com.nwq.adapter.ResStrKeyText
 import com.nwq.base.BaseFragment
@@ -27,6 +28,12 @@ class SelectRegionFragment : BaseFragment<FragmentSelectRegionBinding>() {
 
     private fun getList():List<ResStrKeyText>{
         val  list = mutableListOf<ResStrKeyText>()
+        list.add(ResStrKeyText(R.string.full_screen))
+        list.add(ResStrKeyText(R.string.start_screen))
+        list.add(ResStrKeyText(R.string.select_picture))
+        list.add(ResStrKeyText(R.string.take_img))
+        list.add(ResStrKeyText(R.string.select_critical_area))
+        list.add(ResStrKeyText(R.string.find_the_image_area))
         return list
     }
 }
