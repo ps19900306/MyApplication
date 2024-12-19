591ae85561aea1e0cbe9ee45c858c80ff3b6b8b9
gongduoxiang <gongduoxiang@163.com>
2024-12-19 18:27:05 +0800

    准备新建新的HSV规则图片

--- app/src/main/java/com/example/myapplication/adapter/AutoHsvRuleAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/AutoHsvRuleAdapter.kt
@@ -4,23 +4,23 @@ package com.example.myapplication.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.FragmentAutoHsvRuleListBinding
 import com.example.myapplication.databinding.ItemAutoHsvRuleListBinding
-import com.example.myapplication.databinding.ItemFindTargetBinding
 import com.nwq.opencv.IAutoRulePoint
 
 
 class AutoHsvRuleAdapter : RecyclerView.Adapter<AutoHsvRuleAdapter.ViewHolder>() {
 
     private val list = mutableListOf<IAutoRulePoint>()
-
+    private val mSelectList = mutableListOf<IAutoRulePoint>()
+    private val isSingCheck = false
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemAutoHsvRuleListBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-        return ViewHolder(binding)
+
+        return ViewHolder(binding, mSelectList, isSingCheck)
     }
 
     override fun getItemCount(): Int {
@@ -35,10 +35,21 @@ class AutoHsvRuleAdapter : RecyclerView.Adapter<AutoHsvRuleAdapter.ViewHolder>()
 
 
     inner class ViewHolder(
-        val binding: ItemAutoHsvRuleListBinding
-    ) :
-        RecyclerView.ViewHolder(binding.root) {
+        val binding: ItemAutoHsvRuleListBinding,
+        val selectList: MutableList<IAutoRulePoint>,
+        val isSingCheck: Boolean,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        private var mIAutoRulePoint: IAutoRulePoint? = null
 
+        init {
+            binding.cBox.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isSingCheck){
+
+                }else{
+                    mIAutoRulePoint?.setIsSelected(isChecked)
+                }
+            }
+        }
 
         fun bindData(item: IAutoRulePoint) {
 
