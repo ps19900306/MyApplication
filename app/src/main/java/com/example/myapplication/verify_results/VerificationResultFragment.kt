857e66b13ee68e435fe7303ac7be65b7cffbf4d2
gongduoxiang <gongduoxiang@163.com>
2025-01-06 18:23:55 +0800

    1

---
+++ app/src/main/java/com/example/myapplication/verify_results/VerificationResultFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.verify_results
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentFindTargetDetailBinding
+import com.example.myapplication.databinding.FragmentVerificationResultBinding
+import com.nwq.base.BaseFragment
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [VerificationResultFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class VerificationResultFragment : BaseFragment<FragmentVerificationResultBinding>() {
+    override fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentVerificationResultBinding {
+        TODO("Not yet implemented")
+    }
+
+}
