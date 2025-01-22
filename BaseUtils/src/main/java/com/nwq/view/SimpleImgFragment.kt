5926fc4afcad46b3e09c2334f8f297fa06e7c33d
gongduoxiang <gongduoxiang@163.com>
2025-01-22 18:20:25 +0800

    内容全屏幕了

--- BaseUtils/src/main/java/com/nwq/view/SimpleImgFragment.kt
+++ BaseUtils/src/main/java/com/nwq/view/SimpleImgFragment.kt
@@ -3,11 +3,12 @@ package com.nwq.view
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.nwq.base.BaseDialogFragment
 import com.nwq.baseutils.databinding.FragmentSimpleImgBinding
 
 
-class SimpleImgFragment(val bitmap: Bitmap) : BaseDialogFragment<FragmentSimpleImgBinding>() {
+class SimpleImgFragment(val bitmap1: Bitmap, val bitmap2: Bitmap?=null, val bitmap3: Bitmap?=null) : BaseDialogFragment<FragmentSimpleImgBinding>() {
     override fun createBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -17,7 +18,15 @@ class SimpleImgFragment(val bitmap: Bitmap) : BaseDialogFragment<FragmentSimpleI
 
     override fun initData() {
         //不能外部点击关掉
-        binding.img.setImageBitmap(bitmap)
+        binding.img.setImageBitmap(bitmap1)
+        bitmap2?.let {
+            binding.img2.setImageBitmap(it)
+            binding.img2.isVisible = true
+        }
+        bitmap3?.let {
+            binding.img3.setImageBitmap(it)
+            binding.img3.isVisible = true
+        }
         binding.sureBtn.setOnClickListener {
             dismissDialog()
         }
