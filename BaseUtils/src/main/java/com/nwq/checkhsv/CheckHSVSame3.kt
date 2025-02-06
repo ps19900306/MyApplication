0f4acfc8e513c34a16ecf9012919d1cafa7d58a7
gongduoxiang <gongduoxiang@163.com>
2025-02-06 11:20:15 +0800

    修改代理

--- BaseUtils/src/main/java/com/nwq/checkhsv/CheckHSVSame3.kt
+++ BaseUtils/src/main/java/com/nwq/checkhsv/CheckHSVSame3.kt
@@ -2,14 +2,16 @@ package com.nwq.checkhsv
 
 import kotlin.math.abs
 
-//白色的特别用
+//白色的特别用 eve菜单按钮的颜色
 class CheckHSVSame3 : CheckHSVSame {
 
+
+   //差不多有50的饱和度差距 如果背景是全红的情况下
    override fun checkHSVSame(h1:Double, s1:Double, v1:Double, h2:Double, s2:Double, v2:Double): Boolean{
-       if ( s1 < 30 && s2 < 30 && v1 > 140 && v2 > 140) {
+       if ( s1 < 60 && s2 < 60 && v1 > 110 && v2 > 110) {
            return (abs(h1 - h2) <= 180) &&
-                   (abs(s1 - s2) <= 180) &&
-                   (abs(v1 - v2) <= 180)
+                   (abs(s1 - s2) <= 60) &&
+                   (abs(v1 - v2) <= 60)
        }
        return abs(h1 - h2) <= 1 &&
                abs(s1 - s2) <= 1 &&
