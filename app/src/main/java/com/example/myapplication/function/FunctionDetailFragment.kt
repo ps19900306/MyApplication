026ac9af37f2d9f293d6888dd1d038ee7482852b
Admin <ps19900306@163.com>
2025-05-18 21:40:10 +0800

    准备提交一些

--- app/src/main/java/com/example/myapplication/function/FunctionDetailFragment.kt
+++ app/src/main/java/com/example/myapplication/function/FunctionDetailFragment.kt
@@ -1,24 +1,28 @@
 package com.example.myapplication.function
 
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.myapplication.databinding.FragmentFunctionDetailBinding
-import com.nwq.base.BaseFragment
+import com.nwq.base.BaseToolBarFragment
+import com.example.myapplication.R
 
-
-
-class FunctionDetailFragment : BaseFragment<FragmentFunctionDetailBinding>() {
+class FunctionDetailFragment : BaseToolBarFragment<FragmentFunctionDetailBinding>() {
 
     private val args: FunctionDetailFragmentArgs by navArgs()
     private val viewModel: FunctionEdtViewModel by viewModels()
-    override fun createBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentFunctionDetailBinding {
-        return FragmentFunctionDetailBinding.inflate(inflater, container, false)
+
+
+    override fun getLayoutId(): Int {
+       return R.layout.fragment_function_detail
+    }
+
+    override fun getTitleRes(): Int {
+       return R.string.function_detail
+    }
+
+    override fun onBackPress() {
+        findNavController().popBackStack()
     }
 
     override fun initData() {
